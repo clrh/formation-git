95e9fac085fcca8a6ad91cb38639cb315272812c
Claire <claire.hernandez@biblibre.com>
2011-06-13 14:25:17 +0200

    Initial commit

---
+++ export.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+more ex1_addcommittag_correction | grep "@" > ex1_addcommittag
+more ex2_patchs_correction | grep "@" > ex2_patchs
+more ex3_branches_correction | grep "@" > ex3_branches
+more ex4_remote_correction | grep "@" > ex4_remote
+more ex5_autres_correction | grep "@" > ex5_autres
+
+
